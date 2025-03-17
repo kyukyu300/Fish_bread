7244545e3f91a6e42f6c7e1077665d6c8c64a2e9
kyukyu300 <kyujin1252@gmail.com>
2025-03-17 17:32:39 +0900

    ✨ 개발 브랜치 생성
    모드를 입력받을 수 있는 기능 구현 및 종료 기능 구현

---
+++ Fish_bread.py
@@ -0,0 +1,9 @@
+while True:
+    mode = input("원하는 모드를 선택하세요. (주문, 관리자, 종료): ")
+    if mode == "종료":
+        print("시스템을 종료합니다.")
+        break
+    elif mode == "주문":
+        pass
+    elif mode == "관리자":
+        pass

e257cfe3730ebe1f77076933256bd146417d3006
kyukyu300 <kyujin1252@gmail.com>
2025-03-18 17:51:06 +0900

    ✨ 총 매출 계산해주는 기능 구현
    price 변수에 붕어빵 가격을 저장 후 계산에 활용 기능 구현

--- Fish_bread.py
+++ Fish_bread.py
@@ -1,3 +1,28 @@
+stock = {
+    "팥붕어빵" : 10, 
+    "슈크림붕어빵": 8,
+    "초코붕어빵" : 5
+}
+
+sales = {
+    "팥붕어빵" : 0, 
+    "슈크림붕어빵": 0,
+    "초코붕어빵" : 0
+}
+
+#붕어빵 메뉴판
+price = {
+    "팥붕어빵" : 1000, 
+    "슈크림붕어빵": 1200,
+    "초코붕어빵" : 1500
+}
+
+def calculate_sales():
+    total_sales = 0
+    for key in sales: # 팥붕어빵
+        total_sales += (price[key] * sales[key])
+    print(f'오늘의 총 매출은 {total_sales}원 입니다.')
+
 while True:
     mode = input("원하는 모드를 선택하세요. (주문, 관리자, 종료): ")
     if mode == "종료":
@@ -7,3 +32,5 @@ while True:
         pass
     elif mode == "관리자":
         pass
+
+calculate_sales()
